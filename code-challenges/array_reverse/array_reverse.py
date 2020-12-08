97dde297cdd678e7e88789c7a25c9f0d2eedb1d7
Sian <sianculligan@gmail.com>
2020-12-07 20:48:08 -0800

    forgot an equal sign

--- code-challenges/array_reverse/array_reverse.py
+++ code-challenges/array_reverse/array_reverse.py
@@ -1,9 +1,9 @@
 def arrayReverse(arr):
     arrCount = len(arr)-1
-    while arrCount > 0:
+    while arrCount >= 0:
         print (arr[arrCount])
         arrCount -= 1
 
 
-exampleList: (1,2,3,4,5)
+exampleList = [5,4,3,2,1]
 arrayReverse(exampleList)
